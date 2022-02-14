4fb1a66d3b191e77c7b3ac97a0fc68c020bff563
DESKTOP-RKMKLHA\owner <2825873215@QQ.COM>
2022-02-14 18:23:43 +0800

    add string to time.Time

--- timex/time.go
+++ timex/time.go
@@ -2,6 +2,16 @@ package timex
 
 import "time"
 
+var (
+	dayLayout     = "2006-01-02"
+	secondsLayout = "2006-01-02 15:04:05"
+)
+
 func DayString() string {
-	return time.Now().Format("2006-01-02")
+	return time.Now().Format(dayLayout)
+}
+
+func MustStringToTime(timeStr string) time.Time {
+	t, _ := time.ParseInLocation(secondsLayout, timeStr, time.Local)
+	return t
 }
